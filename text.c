14d6e45edd036395ddbbc8c4db60c3fad473b88e
Richard-afka <2671530264@qq.com>
2025-12-04 23:06:24 +0800

    四则运算

--- text.c
+++ text.c
@@ -1,5 +1,14 @@
 #include<stdio.h>
-int main(){
-    printf("11\n");
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+int divide(int a, int b);
+int main() {
+    int a, b;
+    scanf("%d %d", &a, &b);
+    printf("%d+%d=%d\n",a,b, add(a, b));
+    ptintf("%d-%d=%d\n", a, b, subtract(a, b));
+    ptintf("%d*%d=%d\n", a, b, multiply(a, b));
+    ptintf("%d/%d=%d\n", a, b, divide(a, b));
     return 0;
 }
