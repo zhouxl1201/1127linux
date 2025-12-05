28a78b82d3d3f261509a83ddbcb6a66d05ae9519
lemon1487 <13479393774@163.com>
2025-12-05 20:13:19 +0800

    add multiplication

--- text.c
+++ text.c
@@ -1,7 +1,9 @@
 #include<stdio.h>
 int add(int a, int b);
 int subtract(int a, int b);
-int multiply(int a, int b);
+int multiply(int a, int b){
+	return a*b;
+}
 int divide(int a, int b);
 int main() {
     int a, b;
